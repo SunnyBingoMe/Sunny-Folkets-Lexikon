cb95e06b63843e191fe211aa840c669e20e62bcb
Sunny <SunnyBingoMe@gmail.com>
2012-03-06 18:50:39 +0100

    v1.1.2012-03-06.1850, fixed shell error

--- 4-xml2dsl-wav_mp3.sh
+++ 4-xml2dsl-wav_mp3.sh
@@ -6,6 +6,6 @@ if [ $# != 1 ]; then
 fi
 xsltproc -o tmp xml2dsl-$1_2012-02-29.xsl folkets_sv_en_public_2012-02-26_solved-amp.xml
 echo -n -e \\xEF\\xBB\\xBF > Sunny-Folkets-Lexikon-sv_en-$1-BinSun.dsl
-cat tmp >> Sunny-Folkets-sv_en-$1-BinSun.dsl
+cat tmp >> Sunny-Folkets-Lexikon-sv_en-$1-BinSun.dsl
 rm tmp
 
